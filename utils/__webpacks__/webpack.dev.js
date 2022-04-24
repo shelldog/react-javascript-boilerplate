d58288cbfd17fa55ba66dad614024dcc0745cb34
shelldog <hajimedevlife@gmail.com>
2022-04-24 15:21:35 +0700

    add environment variables reading features to webpack.dev

--- utils/__webpacks__/webpack.dev.js
+++ utils/__webpacks__/webpack.dev.js
@@ -5,33 +5,63 @@
  * */
 
 const path = require('path')
+const dotenv = require('dotenv')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const fs = require('fs')
 
-module.exports = require('./webpack.core.js')({
-  // tell the webpack this is for development.
-  mode: 'development',
+module.exports = ({ ENVIRONMENT }) => {
+  // get the root path.
+  const root = process.cwd()
 
-  // output: use chunk instead of hash for better performance.
-  output: {
-    filename: '[name].chunk.js',
-  },
+  // read the env file.
+  const basePath = root + '/.env'
 
-  // optimization: in development.
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
+  // concatonate the environment.
+  const envPath = basePath + '.' + ENVIRONMENT
+
+  // check if the file is exists, otherwise fall back to the production .env
+  const finalPath = fs.existsSync(envPath) ? envPath : basePath
+
+  // parse with dotenv.
+  const envFile = dotenv.config({ path: finalPath }).parsed
+
+  // return env objects.
+  const envObj = Object.keys(envFile).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(envFile[next])
+    return prev
+  }, {})
+
+  // webpack init.
+  return require('./webpack.core.js')({
+    // tell the webpack this is for development.
+    mode: 'development',
+
+    // output: use chunk instead of hash for better performance.
+    output: {
+      filename: '[name].chunk.js',
     },
-  },
 
-  // plugins: in development.
-  plugins: [
-    // HtmlWebpackPlugin'll inject JavaScript codes into tags and put in HTML tag.
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: path.resolve(process.cwd(), 'public', 'index.html'),
-    }),
-  ],
+    // optimization: in development.
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
 
-  // devtool: boot your development.
-  devtool: 'inline-source-map',
-})
+    // plugins: in development.
+    plugins: [
+      // HtmlWebpackPlugin'll inject JavaScript codes into tags and put in HTML tag.
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: path.resolve(process.cwd(), 'public', 'index.html'),
+      }),
+
+      // DefinePlugin for reading environment variables.
+      new webpack.DefinePlugin(envObj),
+    ],
+
+    // devtool: boot your development.
+    devtool: 'inline-source-map',
+  })
+}
